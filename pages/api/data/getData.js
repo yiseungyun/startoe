ff5d14c1b9e7701a89ee2cbd05ba8bacf480ba06
yiseungyun <seungxxyun@gmail.com>
2024-03-14 21:36:22 +0900

    Fix environment variable for getData

--- pages/api/data/getData.js
+++ pages/api/data/getData.js
@@ -1,6 +1,7 @@
+'use client'
 import axios from "axios"
 
-const database_url = process.env.NEXT_PUBLIC_DATABASE_URL
+const database_url = process.env.DATABASE_URL
 export const getLearningData = async (path, index) => {
   const response = await axios.get(`${database_url}/${path}/${index}.json`);
   response.data.data.sort((a, b) => a.id - b.id);
